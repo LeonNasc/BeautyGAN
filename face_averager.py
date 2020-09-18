badaced98c1a91a661060505b647a1f59c6a4a81
Leon De Franca Nascimento <l.nasc@live.com>
2020-09-18 16:18:55 -0300

    face avg

---
+++ face_averager.py
@@ -0,0 +1,18 @@
+#encoding:utf-8
+import matplotlib.pyplot as plt
+
+from facer import facer
+
+# Load face images
+path_to_images = "./FBP_male"
+images = facer.load_images(path_to_images)
+
+# Detect landmarks for each face
+landmarks, faces = facer.detect_face_landmarks(images)
+
+# Use  the detected landmarks to create an average face
+average_face = facer.create_average_face(faces, landmarks, save_image=True)
+
+# View the composite image
+plt.imshow(average_face)
+plt.show()
